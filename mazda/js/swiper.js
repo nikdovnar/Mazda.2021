61ef5e3a65a87f309a2c7d2f44240e6c4c01a216
Nikita <87595975+nikdovnar@users.noreply.github.com>
2021-10-10 21:51:36 +0300

    Add files via upload

---
+++ mazda/js/swiper.js
@@ -0,0 +1,31 @@
+const swiper1 = new Swiper('.swiper1', {
+	pagination: {
+		el: '.swiper-pagination1',
+		clickable: true,
+	},
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+	autoplay: {
+		delay: 3500,
+		stopOnLastSlide: false,
+		disableOnInteraction: false,
+	}, 
+	keyboard: true,
+	loop: true,
+	speed: 1200,
+});
+const swiper2 = new Swiper ('.swiper__news', {
+	navigation: {
+		nextEl: '.swiper-button-next',
+	},
+	autoplay: {
+		delay: 3500,
+		stopOnLastSlide: false,
+		disableOnInteraction: false,
+	},
+	keyboard: true,
+	loop: true,
+	speed: 1200,
+});  
